e3a4018509136dbcc6c789f4a1a68826e67af1ca
Saksham2k <105886134+Saksham2k@users.noreply.github.com>
2024-02-26 17:12:27 +0530

    Add files via upload

---
+++ G2B2_ReactJS_Graded-LAB-4/expense-tracker/src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import ExpenseTracker from "./Components/ExpenseTracker";
+import ShowData from "./Components/ShowList";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/add"
+          element={<ExpenseTracker onTrue={undefined} onClose={undefined} />}
+        />
+        <Route path="/" element={<ShowData />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
